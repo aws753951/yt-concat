8812498ea12e0d2a4160761e6838980421dd9bdc
MarcZhu <a0983204766@gmail.com>
2022-04-15 15:12:40 +0800

    successfully load dotenv

--- yt_concat/main.py
+++ yt_concat/main.py
@@ -2,16 +2,15 @@ import os
 import urllib.request
 import json
 
-from yt_concat.setting import DOWNLOAD_DIR, VIDEOS_DIR, CAPTIONS_DIR
+from yt_concat.setting import DOWNLOAD_DIR, VIDEOS_DIR, CAPTIONS_DIR, api_key
 
 
 def get_all_video_in_channel(channel_id):
-    api_key = ''
 
     base_video_url = 'https://www.youtube.com/watch?v='
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
-    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
+    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=2'.format(api_key,
                                                                                                         channel_id)
 
     video_links = []
@@ -29,6 +28,7 @@ def get_all_video_in_channel(channel_id):
             url = first_url + '&pageToken={}'.format(next_page_token)
         except KeyError:  # the outcome of trying
             break
+        break
 
     # id = []
     # for line in video_links:
